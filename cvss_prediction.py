0b23712777fa2ffcbe1b292afc328807df4c1512
Rachit Trivedi <rachit@orendasolutions.com>
2020-06-04 13:15:31 -0300

    Added Natural Language Processing

--- cvss_prediction.py
+++ cvss_prediction.py
@@ -2,6 +2,19 @@ import json
 import sys
 import os
 import pandas as pd
+import numpy as np
+import re
+from operator import itemgetter
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import plot_confusion_matrix
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class LoadData:
 
@@ -86,10 +99,177 @@ class LoadData:
 
 		return df
 
+class NLPModel:
+
+	def __init__(self, X_train, X_test, y_train, y_test):
+		self.X_train = X_train
+		self.X_test = X_test
+		self.y_train = y_train
+		self.y_test = y_test
+
+	def clean_vulnerability_description(self, desc):
+		version_pattern_1 = r"((\d+\.)+\d+-\d+\.\d+)" # for patterns like 10.0-10.4
+		version_pattern_2 = r"((\d+\.)+\d+-[\d\w]+)" # for patterns like 7.6.3-rev37 or 7.0.7-24
+		version_pattern_3 = r"((\d+\.)+[\d\w]+)" # for patterns like 5.9.1.10 or 2.2.0
+		file_address_1 = r"(((\w+\.?\w+)\/)+(\w+\.)*\w+)" # for patterns like public/index.php/home/memberaddress/index.html or public/index.php/home/memberaddress/edit/address_id/2.html or libjasper/jpc/jpc_enc.c
+		file_name = r"(\w+\.\w+)" # for matching file names like 2345BdPcSafe.sys
+		file_address_2 = r"((\\\w+\.?\w+)+(\.?\w+)*)" # for patterns like \Lib\Lib\Action\Admin\DataAction.class.php
+		punctuations = r"\.:,()" # for removing punctuations like , or . or ()
+
+		desc = re.sub(r"|".join((version_pattern_1, version_pattern_2, version_pattern_3, file_address_1, file_name, file_address_2, punctuations)), "", desc)
+
+		return desc.lower()
+
+	def print_text_with_target(self, desc, output):
+		print(desc, output)
+
+	def build_model(self):
+		#clean_vulnerability_description_vector = np.vectorize(self.clean_vulnerability_description)(self.X_train)
+		X_train_clean = np.vectorize(self.clean_vulnerability_description)(self.X_train)
+		X_test_clean = np.vectorize(self.clean_vulnerability_description)(self.X_test)
+
+		'''
+		X_train_clean_tokenized = map(PreProcessing.convert_string_to_tokens, X_train_clean)
+		X_test_clean_tokenized = map(PreProcessing.convert_string_to_tokens, X_test_clean)
+		'''
+		self.count_vect = CountVectorizer(
+			preprocessor=lambda x: x,
+			tokenizer = lambda x: x.split(" "),
+			ngram_range = (1, 3)
+		)
+
+		x_train_count_vect = self.count_vect.fit_transform(X_train_clean)
+		x_test_count_vect = self.count_vect.transform(X_test_clean)
+
+		self.lr = LogisticRegression(max_iter = 2000)
+		self.lr.fit(X = x_train_count_vect, y = self.y_train)
+		predictions = self.lr.predict(X = x_test_count_vect)
+		print(predictions)
+
+		return predictions
+
+	def get_top_bottom_features(self, label_encoder_object):
+
+		feature_names = [feature for feature, index in sorted(self.count_vect.vocabulary_.items(), key = itemgetter(1))]
+
+		for i, category in enumerate(self.lr.classes_):
+			coef_i = self.lr.coef_[i]
+			coef_features = sorted(zip(coef_i, feature_names), key = itemgetter(0), reverse = True)
+
+			print(label_encoder_object.inverse_transform([category]))
+			print(coef_features[:20])
+			print(coef_features[:-20:-1])
+
+			Visualizer.visualize_imp_features(top_feature_coef_list = coef_features[:20], bottom_feature_coef_list = coef_features[:-20:-1], title = label_encoder_object.inverse_transform([category])[0])
+
+
+
+	string_to_remove_rows = "** reject **  do not use this candidate number." # description unavailable
+
+	@classmethod
+	def make_is_remove(cls, desc):
+		if(desc.startswith(cls.string_to_remove_rows)):
+			return True
+		return False
+
+	def print_metrics(self, predictions):
+		cm = confusion_matrix(self.y_test, predictions, labels = [0, 1, 2, 3])
+		print(cm)
+
+
+class PreProcessing:
+
+	@staticmethod
+	def convert_target_categorical(continuous_value):
+
+		if(continuous_value >= 0.1 and continuous_value <= 3.99):
+			return "LOW"
+		elif(continuous_value >= 4.0 and continuous_value <= 6.99):
+			return "MEDIUM"
+		elif(continuous_value >= 7.0 and continuous_value <= 8.99):
+			return "HIGH"
+		else:
+			return "CRITICAL"
+
+	@staticmethod
+	def convert_string_to_tokens(description):
+		return description.split(" ")
+
+
+class Visualizer:
+
+	def __init__(self):
+		super().__init__()
+
+	@staticmethod
+	def visualize_imp_features(top_feature_coef_list, bottom_feature_coef_list, title = None):
+		top_features = [i[1] for i in top_feature_coef_list]
+		top_coefficients = [i[0] for i in top_feature_coef_list]
+
+		bottom_features = [i[1] for i in bottom_feature_coef_list]
+		bottom_coefficients = [i[0] for i in bottom_feature_coef_list]
+
+		combined_coefficients = bottom_coefficients[:]
+		combined_coefficients.extend(top_coefficients[::-1])
+		combined_features = bottom_features[:]
+		combined_features.extend(top_features[::-1])
+
+		combined_colours = ["red" if index < len(bottom_coefficients) else "green" for index in range(0, len(combined_coefficients))]
+
+		plt.tight_layout()
+		plt.barh(y = np.arange(start = 0, stop = len(combined_coefficients)), width = combined_coefficients, color = combined_colours)
+		plt.yticks(ticks = np.arange(start = 0, stop = len(combined_features)), labels = combined_features)
+		plt.xlabel("word importance")
+		plt.title(label = title)
+
+		plt.show()
+
+	@staticmethod
+	def show_confusion_matrix(actual_output, predicted_output, label_encoder_object):
+		cm = confusion_matrix(actual_output, predicted_output, labels = [0, 1, 3, 2])
+		df_cm = pd.DataFrame(
+			cm,
+			index = [label_encoder_object.inverse_transform([i])[0] for i in [0, 1, 3, 2]],
+			columns = [label_encoder_object.inverse_transform([i])[0] for i in [0, 1, 3, 2]]
+		)
+
+		print(df_cm)
+
+		sns.heatmap(df_cm, annot = True, fmt = "d")
+		plt.xlabel("Predicted")
+		plt.ylabel("Actual")
+
+		plt.show()
+
 			
 if(__name__ == "__main__"):
 	data_obj_2019 = LoadData(json_file_name = "nvdcve-1.0-2019.json")
 	data_obj_2019.load_file()
 	df_2019 = data_obj_2019.get_dataframe()
 
+	data_obj_2018 = LoadData(json_file_name = "nvdcve-1.0-2018.json")
+	data_obj_2018.load_file()
+	df_2018 = data_obj_2018.get_dataframe()
+
+	df_2018["is_remove"] = df_2018["description"].apply(lambda x: NLPModel.make_is_remove(x))
+	df_2019["is_remove"] = df_2019["description"].apply(lambda x: NLPModel.make_is_remove(x))
+
+	df_2018 = df_2018.loc[~df_2018["is_remove"]]
+	df_2019 = df_2019.loc[~df_2018["is_remove"]]
+
+	df_2018["categorical_target"] = df_2018["target"].apply(lambda x: PreProcessing.convert_target_categorical(x))
+	df_2019["categorical_target"] = df_2019["target"].apply(lambda x: PreProcessing.convert_target_categorical(x))
+
+	le_obj = LabelEncoder()
+	df_2018["categorical_target"] = le_obj.fit_transform(df_2018["categorical_target"].values)
+	df_2019["categorical_target"] = le_obj.transform(df_2019["categorical_target"].values)
+
+	print(df_2018)
 	print(df_2019)
+
+	nlp_model_object = NLPModel(X_train = df_2018["description"].values, X_test = df_2019["description"].values, y_train = df_2018["categorical_target"].values, y_test = df_2019["categorical_target"].values)
+	predictions = nlp_model_object.build_model()
+	nlp_model_object.print_metrics(predictions = predictions)
+	nlp_model_object.get_top_bottom_features(le_obj)
+
+	Visualizer.show_confusion_matrix(df_2019["categorical_target"].values, predictions, label_encoder_object = le_obj)
